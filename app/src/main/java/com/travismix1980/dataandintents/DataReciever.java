4c9aebe001359bdd000221a9f8845c47562ed322
Travis Mix <travismix1980@gmail.com>
2019-08-21 22:32:02 -0700

    Main Layout Finished Data Reciever Layout started

---
+++ app/src/main/java/com/travismix1980/dataandintents/DataReciever.java
@@ -0,0 +1,14 @@
+package com.travismix1980.dataandintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataReciever extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_reciever);
+    }
+}
