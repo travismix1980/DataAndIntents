61d29a67adf9a33980bc4681f9406630b9d504b4
Travis Mix <travismix1980@gmail.com>
2019-08-21 22:16:47 -0700

    Initial Commit

---
+++ app/src/main/java/com/travismix1980/dataandintents/MainActivity.java
@@ -0,0 +1,14 @@
+package com.travismix1980.dataandintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
